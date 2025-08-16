b4757a7b6d7f11002f1c49ad9b1a5c10798b91f7
parasara-Asad-7332 <kingofparasara786@gmail.com>
2025-08-16 08:44:20 +0530

    Add files via upload

---
+++ sub.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int sum(int a,int b){
+    return a - b ;
+}
+
+int main(){
+    int a,b;
+    printf("Enter value of a is: ");
+    scanf("%d",&a);
+    printf("Enter value of b is: ");
+    scanf("%d",&b);
+    printf("This %d and %d value of sub is: %d",a,b,sum(a,b));
+    
+    return 0;
+}
