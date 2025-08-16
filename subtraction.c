5a36c08fc1b18a252b1f6d5aa5649289d9dee5fb
parasara-Asad-7332 <kingofparasara786@gmail.com>
2025-08-16 08:47:29 +0530

    Update and rename sub.c to subtraction.c

--- subtraction.c
+++ subtraction.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int sum(int a,int b){
+int sub(int a,int b){
     return a - b ;
 }
 
@@ -10,7 +10,8 @@ int main(){
     scanf("%d",&a);
     printf("Enter value of b is: ");
     scanf("%d",&b);
-    printf("This %d and %d value of sub is: %d",a,b,sum(a,b));
+    printf("This %d and %d value of sub is: %d",a,b,sub(a,b));
     
     return 0;
+
 }
